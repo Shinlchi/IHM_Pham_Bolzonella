a29a9203ff2fae4d04b06f1399f2c534ec3889f5
Temosare <44861968+Temosare@users.noreply.github.com>
2018-12-13 00:34:10 +0100

    latest patch css
    miss magazine and modification atelier

--- RollHab/src/ch/makery/rollhab/MainApp.java
+++ RollHab/src/ch/makery/rollhab/MainApp.java
@@ -2,72 +2,327 @@ package ch.makery.rollhab;
 
 import java.io.IOException;
 
+import ch.makery.rollhab.MainApp;
+import ch.makery.rollhab.view.Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+//import javafx.stage.Modality;
 import javafx.stage.Stage;
+
 
 public class MainApp extends Application {
 
-    private Stage primaryStage;
-    private BorderPane AtelierLayout;
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	Controller controller ;
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Belingo");
 
-    @Override
-    public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("RollHab");
+		initRootLayout();
+		showConnexionOverview();
+	}
 
-        initAtelierLayout();
-        showAtelierOverviewDragDrop();
-    }
-    
-    /**
-     * Initializes the root layout.
-     */
-    public void initAtelierLayout() {
-        try {
-            // Load root layout from fxml file.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/AtelierLayout.fxml"));
-            AtelierLayout = (BorderPane) loader.load();
-            
-            // Show the scene containing the root layout.
-            Scene scene = new Scene(AtelierLayout);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	/**
+	 * Initializes the connexion layout.
+	 */
+	public void initRootLayout() {
+		try {
+			// Load connexion layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/rootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
 
-    /**
-     * Shows the person overview inside the root layout.
-     */
-    public void showAtelierOverviewDragDrop() {
+			// Show the scene containing the connexion layout.
+			Scene scene = new Scene(rootLayout);
+
+
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	/**
+	 * Shows the connexion overview inside the root layout.
+	 */
+	public void showConnexionOverview() {
+		try {
+			// Load connexion overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/ConnexionOverview.fxml"));
+			AnchorPane ConnexionOverview = (AnchorPane) loader.load();
+
+			// Set person overview into the center of root layout.
+			rootLayout.setCenter(null);
+			rootLayout.setBottom(null);
+			rootLayout.setTop(null);
+			rootLayout.setLeft(null);
+			rootLayout.setRight(null);
+			rootLayout.setCenter(ConnexionOverview);
+			// Give the controller access to the main app.
+			controller = loader.getController();
+			controller.setMainApp(this);
+
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void showConnexionUserOverview() {
+		try {
+			// Load connexion user overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/ConnexionUserOverview.fxml"));
+			AnchorPane ConnexionUserOverview = (AnchorPane) loader.load();
+
+			// Set person overview into the center of root layout.
+			rootLayout.setCenter(null);
+			rootLayout.setBottom(null);
+			rootLayout.setTop(null);
+			rootLayout.setLeft(null);
+			rootLayout.setRight(null);
+			rootLayout.setCenter(ConnexionUserOverview);
+
+			// Give the controller access to the main app.
+			controller = loader.getController();
+			controller.setMainApp(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void showHomeLayout() {
+		try {
+			// Load connexion user overview.
+			
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/HomeLayout.fxml"));
+			AnchorPane HomeLayout = (AnchorPane) loader.load();
+			
+			//rootLayout.clearConstraints();
+			//BorderPane.clearConstraints(rootLayout);
+			rootLayout.setCenter(null);
+			rootLayout.setBottom(null);
+			rootLayout.setTop(null);
+			rootLayout.setLeft(null);
+			rootLayout.setRight(null);
+			
+			rootLayout.setTop(HomeLayout);
+			
+			// Give the controller access to the main app.
+			controller = loader.getController();
+			controller.setMainApp(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void showHomeOverview() {
+		try {
+			// Load connexion user overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/HomeOverview.fxml"));
+			AnchorPane HomeOverview = (AnchorPane) loader.load();
+			
+			rootLayout.setCenter(null);
+			rootLayout.setBottom(null);
+			
+			rootLayout.setLeft(null);
+			rootLayout.setRight(null);
+			rootLayout.setCenter(HomeOverview);
+			
+			// Give the controller access to the main app.
+			controller = loader.getController();
+			controller.setMainApp(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void showAtelierLayout() {
+		try {
+			// Load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/AtelierLayout.fxml"));
+			AnchorPane AtelierLayout = (AnchorPane) loader.load();
+
+			// Set person overview into the center of root layout.
+			rootLayout.setCenter(null);
+			rootLayout.setBottom(null);
+			rootLayout.setTop(null);
+			rootLayout.setLeft(null);
+			rootLayout.setRight(null);
+			rootLayout.setTop(AtelierLayout);
+
+			// Give the controller access to the main app.
+			controller = loader.getController();
+			controller.setMainApp(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public void showAtelierOverviewRepeat() {
+		try {
+			// Load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/AtelierOverviewRepeat.fxml"));
+			AnchorPane AtelierOverviewRepeat = (AnchorPane) loader.load();
+
+			// Set person overview into the center of root layout.
+			rootLayout.setCenter(null);
+			rootLayout.setBottom(null);
+			
+			rootLayout.setLeft(null);
+			rootLayout.setRight(null);
+			rootLayout.setBottom(AtelierOverviewRepeat);
+
+			// Give the controller access to the main app.
+			controller = loader.getController();
+			controller.setMainApp(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void showHomeAtelierOverview() {
+		try {
+			// Load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/HomeAtelierOverview.fxml"));
+			AnchorPane HomeAtelierOverview = (AnchorPane) loader.load();
+
+			// Set person overview into the center of root layout.
+			rootLayout.setCenter(null);
+			rootLayout.setBottom(null);
+			
+			rootLayout.setLeft(null);
+			rootLayout.setRight(null);
+			rootLayout.setBottom(HomeAtelierOverview);
+
+			// Give the controller access to the main app.
+			controller = loader.getController();
+			controller.setMainApp(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void showMagazineChoiceOverview() {
+		try {
+			// Load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/MagazineChoiceOverview.fxml"));
+			AnchorPane MagazineChoiceOverview = (AnchorPane) loader.load();
+
+			// Set person overview into the center of root layout.
+			rootLayout.setCenter(null);
+			rootLayout.setBottom(null);
+		
+			rootLayout.setLeft(null);
+			rootLayout.setRight(null);
+			rootLayout.setCenter(MagazineChoiceOverview);
+
+			// Give the controller access to the main app.
+			controller = loader.getController();
+			controller.setMainApp(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void showToolsOverviewLexique() {
         try {
             // Load person overview.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/AtelierOverviewDragDrop.fxml"));
-            AnchorPane AtelierOverviewDragDrop = (AnchorPane) loader.load();
+            loader.setLocation(MainApp.class.getResource("view/ToolsOverviewLexique.fxml"));
+            AnchorPane ToolsOverviewLexique = (AnchorPane) loader.load();
             
             // Set person overview into the center of root layout.
-            AtelierLayout.setCenter(AtelierOverviewDragDrop);
+			rootLayout.setCenter(null);
+			rootLayout.setBottom(null);
+			rootLayout.setLeft(null);
+			
+			rootLayout.setRight(null);
+            rootLayout.setCenter(ToolsOverviewLexique);
+            
+            // Give the controller access to the main app.
+	        controller = loader.getController();
+	        controller.setMainApp(this);
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
-    /**
-     * Returns the main stage.
-     * @return
-     */
-    public Stage getPrimaryStage() {
-        return primaryStage;
+	
+	public void showToolsOverviewVerbes() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/ToolsOverviewVerbes.fxml"));
+            AnchorPane ToolsOverviewVerbes = (AnchorPane) loader.load();
+            
+            // Set person overview into the center of root layout.
+			rootLayout.setCenter(null);
+			rootLayout.setBottom(null);
+			rootLayout.setLeft(null);
+			
+			rootLayout.setRight(null);
+            rootLayout.setCenter(ToolsOverviewVerbes);
+            
+            // Give the controller access to the main app.
+	        controller = loader.getController();
+	        controller.setMainApp(this);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+	
+	public void showToolsOverviewTemps() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/ToolsOverviewTemps.fxml"));
+            AnchorPane ToolsOverviewTemps = (AnchorPane) loader.load();
+            
+            // Set person overview into the center of root layout.
+			rootLayout.setCenter(null);
+			rootLayout.setBottom(null);
+			rootLayout.setLeft(null);
+			
+			rootLayout.setRight(null);
+            rootLayout.setCenter(ToolsOverviewTemps);
+            
+            // Give the controller access to the main app.
+	        controller = loader.getController();
+	        controller.setMainApp(this);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	
+	
+	
+	/**
+	 * Returns the main stage.
+	 * @return
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
---
+++ RollHab/src/ch/makery/rollhab/view/Controller.java
@@ -0,0 +1,94 @@
+package ch.makery.rollhab.view;
+
+import javafx.fxml.FXML;
+import ch.makery.rollhab.MainApp;
+
+public class Controller {
+	
+	// Reference to the main application.
+	private MainApp mainApp;
+
+	/**
+	 * The constructor.
+	 * The constructor is called before the initialize() method.
+	 */
+	public Controller() {
+	}
+
+	/**
+	 * Initializes the controller class. This method is automatically called
+	 * after the fxml file has been loaded.
+	 */
+	@FXML
+	private void initialize() {
+		
+	}
+
+	/**
+	 * Is called by the main application to give a reference back to itself.
+	 * 
+	 * @param mainApp
+	 */
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+	}
+
+	/**
+	 * Called when the user clicks the connexion button. Opens a overview.
+	 */
+	@FXML
+	private void handleConnexion() {
+	  
+	    mainApp.showConnexionUserOverview();
+	}
+	@FXML
+	private void handleConfirmer() {
+	  
+	    mainApp.showConnexionUserOverview();
+	}
+	@FXML
+	private void handleUser() {
+		mainApp.showHomeLayout();
+	    mainApp.showHomeOverview();
+	}
+	@FXML
+	private void handleAtelier() {
+		mainApp.showAtelierLayout();
+	    mainApp.showAtelierOverviewRepeat();
+	}
+	@FXML
+	private void handleToutesLesLecon() {
+
+	    mainApp.showHomeAtelierOverview();
+	}
+	@FXML
+	private void handleMagazine() {
+		mainApp.showMagazineChoiceOverview();
+	}
+	
+	
+	
+	@FXML
+	private void handleToolsLexique() {
+		mainApp.showHomeLayout();
+		mainApp.showToolsOverviewLexique();
+		
+	}
+	
+	@FXML
+	private void handleToolsVerbes() {
+
+		mainApp.showHomeLayout();
+		mainApp.showToolsOverviewVerbes();
+		
+	}
+	
+	@FXML
+	private void handleToolsTemps() {
+		mainApp.showHomeLayout();
+		mainApp.showToolsOverviewTemps();
+		
+	}
+	
+	
+}
